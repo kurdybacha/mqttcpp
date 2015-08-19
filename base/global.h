b459eadef614b6588a565eb751a22c42377a2d9e
Pawel Kurdybacha <kladku@gmail.com>
2015-08-19 20:07:07 +0100

    Rename project to mqttcpp

--- base/global.h
+++ base/global.h
@@ -1,8 +1,8 @@
 #ifndef _DL_GLOBAL_H
 #define _DL_GLOBAL_H
 
-#define DL_NAMESPACE dl
-#define DL_BEGIN_NAMESPACE namespace dl {
+#define DL_NAMESPACE mqttcpp
+#define DL_BEGIN_NAMESPACE namespace DL_NAMESPACE {
 #define DL_END_NAMESPACE } 
 
 #endif // _DL_GLOBAL_H
